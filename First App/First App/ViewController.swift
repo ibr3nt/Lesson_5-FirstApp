bc9729d4707ef81825eca925a4e3ef9923470c96
iBrent <ibrent@dev-macmini.lan>
2017-09-20 20:54:51 -0600

    Initial commit of original project files.

---
+++ First App/First App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by iBrent on 8/26/17.
+//  Copyright © 2017 iBrent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
